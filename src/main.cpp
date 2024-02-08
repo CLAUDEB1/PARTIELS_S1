80061fed74ea73713e9164c37a32f31a5ec06506
CLAUDEB1 <claudebenjamin255@gmail.com>
2024-02-08 09:30:14 +0000

    premier commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  const float coutHoraire = 4.0;
+  int velosDispos = 20;
+  int capacite= 20;
+  int choix;
+
+  cout << "****** Gestion d'une station Vélô ******" << endl << endl;
+  cout << "______________________________" << endl;
+  
